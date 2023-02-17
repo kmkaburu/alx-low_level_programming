0b0ee4c1d275915a9406e5a04c6673fdfdaaa6bf
kmkaburu <muchuikelvin@gmail.com>
2023-02-16 18:21:58 -0800

    good friday

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program
- * return - 0(Addict)
+ * main - friday is good
+ * Return: always 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
