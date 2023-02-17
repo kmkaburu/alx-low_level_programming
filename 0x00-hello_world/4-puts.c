6442b46fecc67987ed32e081fcb230f4b7a388bc
kmkaburu <muchuikelvin@gmail.com>
2023-02-16 16:56:25 -0800

    good thursday 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Cancer cure
- *
- * Return: (Take med) 0
+ *  Return: 0 (take med)
  */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle); return (0);
-
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
