e105bc82a1c3693beebe80a635d6f837388dcdfd
kmkaburu <muchuikelvin@gmail.com>
2023-02-16 16:19:20 -0800

    good thursday 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,2 @@
-include <stdio.h>
-"good thursday 5"
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle"); return (0);}
+{ puts("\"Programming is like building a multilingual puzzle); return (0);}
