55d689f95d45fc1f9bfd9bb39f3f8f1fff757194
kmkaburu <muchuikelvin@gmail.com>
2023-02-16 17:15:33 -0800

    good frinday

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program
- * Return: 0 (Success)
- */
+* main - A program
+* Return: 0 (Success)
+*/
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
