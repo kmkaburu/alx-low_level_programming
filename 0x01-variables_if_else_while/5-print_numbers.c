3ea5a9f78971e8b8de0c135443138511fabe3899
kmkaburu <muchuikelvin@gmail.com>
2023-02-19 11:36:54 -0800

    sunday 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Enjoying sunday
+ *Description: With my cousins
+ *base 10 from 0
+ *Return: Always 0 (Sunday)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
