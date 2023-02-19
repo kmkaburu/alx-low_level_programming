41f0ebf6841b5d60ca5a43fc2bb006d85f743952
kmkaburu <muchuikelvin@gmail.com>
2023-02-19 10:54:43 -0800

    sunday 5

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Enjoying sunday
+ *Description: Do the project well
+ *Return: Always 0 (Practise)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
