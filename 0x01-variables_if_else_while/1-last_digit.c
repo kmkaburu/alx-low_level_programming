bbea4e4ca2c4d08f3b25d961a3e1f5543184b5ca
kmkaburu <muchuikelvin@gmail.com>
2023-02-19 04:56:31 -0800

    good sunday 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,15 +7,14 @@
  *Description: Always attend
  * Return: Always 0 (Bible)
  */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	m = n % 10;
+        m = n % 10;
 	if (m > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
