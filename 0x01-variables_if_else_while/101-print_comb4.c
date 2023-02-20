c3ae51a5eb5cffbfb877012419affdcfec2eb68b
kmkaburu <muchuikelvin@gmail.com>
2023-02-20 15:07:12 -0800

    tiresome monday

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - combinations of three digit
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int c;
+	int i;
+	int k;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (i = 0; i <= 10; i++)
+		{
+			for (k = 0; k <= 10; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c + '0');
+					putchar(i + '0');
+					putchar(k + '0');
+
+					if (c + i + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
