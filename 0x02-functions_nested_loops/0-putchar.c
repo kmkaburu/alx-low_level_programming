df9fd5fbfeb42cf36f3e66b2398a52a9901524fd
kmkaburu <muchuikelvin@gmail.com>
2023-02-22 04:50:38 -0800

    km kaburu 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - Main Print
+ *_putchar: print small letters
+ *Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('a');
+	_putchar('b');
+	_putchar('c');
+	_putchar('d');
+	_putchar('e');
+	_putchar('f');
+	_putchar('g');
+	_putchar('\n');
+	return (0);
+}
